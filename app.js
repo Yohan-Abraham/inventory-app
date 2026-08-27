0de842cd399009e2004d076267589e66122ac5fa
Yohan Quraishy <116612238+Yohan-Abraham@users.noreply.github.com>
2026-08-26 17:49:27 -0700

    chore: setup base routing structure

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const path = require('node:path');
+const indexRouter = require('./routes/indexRouter');
+const movieRouter = require('./controllers/movieController');
+const genreRouter = require('./controllers/genreController');
+
+// app.js
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/movies', movieRouter);
+app.use('./genres', genreRouter);
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Listening to port ${port}`);
+});
---
+++ routes/indexRouter.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+const router = Router();
+const controller = require('../controllers/indexController');
+
+router.get('/');
